0d3138a88e96567acb45719eaf79e07155bdbd29
Nisarg118 <nisargbhamat12@gmail.com>
2025-04-07 22:35:27 +0530

    Dashboard Controller

--- src/controller/dashboard.controller.js
+++ src/controller/dashboard.controller.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Aggregate } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { Subscription } from "../models/subscription.model.js";
 import { Like } from "../models/like.model.js";
@@ -10,11 +10,80 @@ const getChannelStats = asyncHandler(async (req, res) => {
   // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
   const userId = req.user._id;
-  
+
+  const stats = await Video.aggregate([
+    {
+      $match: {
+        owner: mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalVideos: { $sum: 1 },
+        totalViews: { $sum: "$views" },
+        totalDuration: { $sum: "$duration" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        totalVideos: 1,
+        totalViews: 1,
+        totalDuration: 1,
+      },
+    },
+  ]);
+
+  const topVideo = await Video.findOne({
+    owner: userId,
+  })
+    .sort({ views: -1 })
+    .select("_id title views thumbnail");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { stats, topVideo },
+        "Channel stats fetched successfully"
+      )
+    );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   // TODO: Get all the videos uploaded by the channel
+
+  const userId = req.user._id;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const videos = await Video.aggregate([
+    {
+      $match: {
+        owner: mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $project: {
+        videoFile: 1,
+        thumbnail: 1,
+        title: 1,
+        description: 1,
+        views: 1,
+        owner: 1,
+      },
+    },
+    { $sort: { createdAt: -1 } },
+    { $skip: skip },
+    { $limit: limit },
+  ]);
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, videos, "Videos fetched successfully"));
 });
 
 export { getChannelStats, getChannelVideos };
